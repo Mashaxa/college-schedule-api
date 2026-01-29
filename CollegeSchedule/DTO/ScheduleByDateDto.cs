cb86d3915d47864ab5208e279812a4f790c5347c
Mashaxa <riaswaag@gmail.com>
2026-01-29 19:33:04 +0700

    Реализовала бизнес логику(создала servis по принципу интерфейс+класс и зарегистрировала servis)

---
+++ CollegeSchedule/DTO/ScheduleByDateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CollegeSchedule.DTO
+{
+    public class ScheduleByDateDto //содержит дату и список занятий на этот день
+    {
+        public DateTime LessonDate { get; set; }
+        public string Weekday { get; set; } = null!;
+        public List<LessonDto> Lessons { get; set; } = new();
+    }
+}
