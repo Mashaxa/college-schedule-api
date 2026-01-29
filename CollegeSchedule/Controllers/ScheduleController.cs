b0dd05a8d970210b5dbbf9f68dd59c93bc525789
Mashaxa <riaswaag@gmail.com>
2026-01-29 19:53:22 +0700

    Создала Api-контроллер(передача сервиса расписания в контроллер), добавила метод get для получения расписания

---
+++ CollegeSchedule/Controllers/ScheduleController.cs
@@ -0,0 +1,28 @@
+﻿using CollegeSchedule.Data;
+using CollegeSchedule.Models;
+using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+        //GET: api/shedule/group/{groupName}?start=2026-01-29
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            //Вызов бизнес-логики из сервиса
+            var result = await _service.GetScheduleForGroup(groupName,start.Date,end.Date);
+            //Возвращаем результат со статусом 200 ОК
+            return Ok(result);
+        }
+    }
+}
